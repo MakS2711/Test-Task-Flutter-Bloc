cf6f2b9f728862e428ff356a4364725e9b79455c
Maxim Dorofeev <Mektogon385@mail.ru>
2021-09-02 21:33:19 +0700

    Added a link

--- lib/features/data/services/foods_api_provider.dart
+++ lib/features/data/services/foods_api_provider.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 class FoodsApiProvider {
   Future<List<Foods>>  getFoods() async {
-    final response = await http.get(Uri.parse('https://raw.githubusercontent.com/MakS2711/json/main/foods.json'));
+    final response = await http.get(Uri.parse('https://raw.githubusercontent.com/MakS2711/TestTaskFlutterBlocBK/main/assets/json/foods.json'));
 
     if (response.statusCode == 200) {
       final List<dynamic> foodsJson = json.decode(response.body);
